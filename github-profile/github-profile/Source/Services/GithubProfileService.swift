f30ed32a18f3bf2cd89ae48b5c42c2b06202a75b
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2021-05-18 23:33:12 -0300

    Refactor GithubProfileService to use async

--- github-profile/github-profile/Source/Services/GithubProfileService.swift
+++ github-profile/github-profile/Source/Services/GithubProfileService.swift
@@ -22,43 +22,25 @@ final class GithubProfileService {
     private let client = NetworkingClient()
     private var fetchCall: CancellableCall?
     
+    private var userProfileEndpoint: String {
+        "\(Constants.rootGithubEndpoint)\(Constants.usersGithubPath)/\(Constants.sampleUser)"
+    }
+    
     // MARK: Public API
     
-    public func fetchProfile(completionHandler: @escaping (Result<User, AppError>) -> Void) {
-        let userProfileEndpoint = "\(Constants.rootGithubEndpoint)\(Constants.usersGithubPath)/\(Constants.sampleUser)"
-        
+    public func fetchProfile() async throws -> User {
         guard let url = URL(string: userProfileEndpoint) else {
             preconditionFailure("Malformed URL")
         }
         
-        fetchCall?.cancel()
-        fetchCall = client.performHttpCall(at: url) { [weak self] result in
-            guard let self = self else {
-                return
-            }
-            
-            switch result {
-            case .success(let data):
-                guard let data = data else {
-                    completionHandler(.failure(.emptyResponse))
-                    return
-                }
-                
-                do {
-                    let user = try self.parseToUser(data)
-                    completionHandler(.success(user))
-                } catch {
-                    debugPrint(error)
-                    completionHandler(.failure(.parsingFailure))
-                }
-                
-            case .failure:
-                completionHandler(.failure(.fetchFailure))
-            }
+        guard let data = try await client.performHttpCall(at: url) else {
+            throw AppError.emptyResponse
         }
+        
+        return try parseDataToUser(data)
     }
     
-    private func parseToUser(_ data: Data) throws -> User {
+    private func parseDataToUser(_ data: Data) throws -> User {
         let decoder = JSONDecoder()
         return try decoder.decode(User.self, from: data)
     }
